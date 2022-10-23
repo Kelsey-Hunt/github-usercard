e25ab8032e5921a2a794c58826aa33b78be11da7
Kelsey-Hunt <kels.hunt13@gmail.com>
2022-10-23 15:28:15 -0500

    initial commit / imported data from github via axios

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,15 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get("https://api.github.com/users/Kelsey-Hunt")
+  .then(resp => {
+    console.log(resp);
+  })
+  .catch(err => console.error(err))
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
